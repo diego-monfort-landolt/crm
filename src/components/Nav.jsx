6c2eb65f35f9320d0335a3d893e40e2cd359f75a
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-03-02 17:55:59 +0100

    a

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,19 @@
-
+import { useNavigate } from "react-router-dom"
 
 const Nav = () => {
+
+  const navigate = useNavigate()
   return (
     <>
-    <h2>Nav</h2>
-    
+      <nav>
+        <div className="logo-container">
+          <img className="logo-img" src={'https://cdn.pixabay.com/photo/2016/08/26/15/54/checklist-1622517_1280.png'} alt="Todoo block - checket list" />
+        </div>
+        <div className="controls-container">
+          <div className="nav-icon" onClick={() => navigate('/ticket')}>🔙</div>
+          <div className="nav-icon" onClick={() => navigate('/')}>❌</div>
+        </div>
+      </nav>
     </>
   )
 }
