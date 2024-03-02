370416d29ab433f2e14eb04b419edde6e599ee27
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-03-02 18:53:23 +0100

    a

--- src/components/TicketCard.jsx
+++ src/components/TicketCard.jsx
@@ -1,7 +1,22 @@
-
+import AvatarDisplay from './AvatarDisplay'
+import StatusDisplay from './StatusDisplay'
+import PriorityDisplay from './PriorityDisplay'
+import ProgressDisplay from './ProgressDisplay'
+import DeleteBlock from './DeleteBlock'
+import { Link } from 'react-router-dom'
 const TicketCard = () => {
   return (
-    <div>TicketCard</div>
+    <div className="ticket-card">
+      <div className='ticket-color'></div>
+      <Link>
+      <h2>Title</h2>
+      <AvatarDisplay />
+      <StatusDisplay />
+      <PriorityDisplay />
+      <ProgressDisplay />
+      <DeleteBlock />
+      </Link>
+    </div>
   )
 }
 
