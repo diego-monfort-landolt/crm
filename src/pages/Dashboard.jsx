0359a5d39d4530c2a4e889f2bb3be32821a55ccd
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-03-05 19:15:39 +0100

    a

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,15 +2,15 @@ import TicketCard from '../components/TicketCard'
 
 const Dashboard = () => {
 
-  // const tickets = [
-  //   {
-  //     category: 'Q1 2023',
-  //     color: 'red',
-  //     title: 'Windows GreenShot is not Working',
-  //     owner: 'DiegoDev',
-  //     avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=ShortHairTheCaesar&accessoriesType=Prescription02&hairColor=BrownDark&facialHairType=BeardLight&facialHairColor=Black&clotheType=Hoodie&clotheColor=Gray02&eyeType=Happy&eyebrowType=FlatNatural&mouthType=Default&skinColor=Brown' 
-  //   }
-  // ]
+  const tickets = [
+    {
+      category: 'Q1 2023',
+      color: 'red',
+      title: 'Windows GreenShot is not Working',
+      owner: 'DiegoDev',
+      avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=ShortHairTheCaesar&accessoriesType=Prescription02&hairColor=BrownDark&facialHairType=BeardLight&facialHairColor=Black&clotheType=Hoodie&clotheColor=Gray02&eyeType=Happy&eyebrowType=FlatNatural&mouthType=Default&skinColor=Brown' 
+    }
+  ]
   return (
     <>
       <div className="dashboard-container">
