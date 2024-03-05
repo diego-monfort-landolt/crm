3e45c33588ff3351d77b22bbbe56b184416d7257
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-03-05 19:39:16 +0100

    added tesxt

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -26,6 +26,18 @@ const Dashboard = () => {
       progress: 10,
       description: 'The button dont works...',
       timeestamp: '2023-12-22 T09:38:17+0000'
+    },
+    {
+      category: 'Q3 2023',
+      color: 'green',
+      title: 'build a bot',
+      owner: 'DiegoDev',
+      avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=ShortHairTheCaesar&accessoriesType=Prescription02&hairColor=BrownDark&facialHairType=BeardLight&facialHairColor=Black&clotheType=Hoodie&clotheColor=Gray02&eyeType=Happy&eyebrowType=FlatNatural&mouthType=Default&skinColor=Brown',
+      status: 'done',
+      priority: 1,
+      progress: 2,
+      description: 'work on it',
+      timeestamp: '2024-01-10 T09:38:17+0000'
     }
   ]
   return (
