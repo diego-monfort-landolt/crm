c04dbc863d12ddabdf8ec8b5bd20b92052be31af
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-03-05 19:36:14 +0100

    added test

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,7 +8,24 @@ const Dashboard = () => {
       color: 'red',
       title: 'Sales Button is not Working',
       owner: 'DiegoDev',
-      avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=ShortHairTheCaesar&accessoriesType=Prescription02&hairColor=BrownDark&facialHairType=BeardLight&facialHairColor=Black&clotheType=Hoodie&clotheColor=Gray02&eyeType=Happy&eyebrowType=FlatNatural&mouthType=Default&skinColor=Brown' 
+      avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=ShortHairTheCaesar&accessoriesType=Prescription02&hairColor=BrownDark&facialHairType=BeardLight&facialHairColor=Black&clotheType=Hoodie&clotheColor=Gray02&eyeType=Happy&eyebrowType=FlatNatural&mouthType=Default&skinColor=Brown',
+      status: 'done',
+      priority: 5,
+      progress: 50,
+      description: 'video how to use the buttons',
+      timeestamp: '2023-12-01 T07:38:17+0000'
+    },
+    {
+      category: 'Q2 2023',
+      color: 'blue',
+      title: 'Home Button is not Working',
+      owner: 'DiegoDev',
+      avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=ShortHairTheCaesar&accessoriesType=Prescription02&hairColor=BrownDark&facialHairType=BeardLight&facialHairColor=Black&clotheType=Hoodie&clotheColor=Gray02&eyeType=Happy&eyebrowType=FlatNatural&mouthType=Default&skinColor=Brown',
+      status: 'done',
+      priority: 2,
+      progress: 10,
+      description: 'The button dont works...',
+      timeestamp: '2023-12-22 T09:38:17+0000'
     }
   ]
   return (
