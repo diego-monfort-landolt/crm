f51273bb6aa707c9d2e456bfe8e8dd16abbefccc
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-03-12 18:29:28 +0100

    added new colors for the stars

--- src/components/PriorityDisplay.jsx
+++ src/components/PriorityDisplay.jsx
@@ -4,10 +4,10 @@ const PriorityDisplay = ({ priority }) => {
     <div className="priority-display">
       <div className="star-container">
         <h3 style={{color: priority >=1 ? 'rgb(255, 0, 0)' : ''}}>★</h3>
-        <h3 style={{color: priority >=2 ? 'rgb(160,32,240)' : ''}}>★</h3>
-        <h3 style={{color: priority >=3 ? 'rgb(255, 95, 21)' : ''}}>★</h3>
-        <h3 style={{color: priority >=4 ? 'rgb(0,255,0)' : ''}}>★</h3>
-        <h3 style={{color: priority >=5 ? 'rgb(0,255,0)' : ''}}>★</h3>
+        <h3 style={{color: priority >=2 ? 'rgb(255, 0, 0)' : ''}}>★</h3>
+        <h3 style={{color: priority >=3 ? 'rgb(255, 0, 0)' : ''}}>★</h3>
+        <h3 style={{color: priority >=4 ? 'rgb(255, 0, 0)' : ''}}>★</h3>
+        <h3 style={{color: priority >=5 ? 'rgb(255, 0, 0)' : ''}}>★</h3>
       </div>
     </div>
   )
