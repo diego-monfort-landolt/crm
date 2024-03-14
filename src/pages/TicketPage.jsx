e7cd37745dd58e984d835d96053acb1ae1e29d60
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-03-14 20:11:00 +0100

    added handlechange

--- src/pages/TicketPage.jsx
+++ src/pages/TicketPage.jsx
@@ -1,17 +1,45 @@
+import { useState } from "react";
 
 const TicketPage = () => {
+  const [formData, setFormData] = useState({
+    status: 'not Started',
+    progress: 0,
+    timestamp: new Date().toISOString()
+  })
   const editMode = false;
 
   const handleSubmit = () => {
     console.log('submitted')
   }
+  const handleChange = (e) => {
+    const value = e.target.value
+    const name = e.target.name
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]:value
+
+    })
+    )
+  }
+  console.log(formData)
 
   return (
     <div className="ticket">
       <h1>{editMode ? 'Update your Ticket' : 'Create a Ticket!' }</h1>
       <div className="ticket-container" >
         <form onSubmit={handleSubmit}>
-
+          <section>
+            <label htmlFor="title">Title</label>
+            <input 
+            id="title" 
+            type="text" 
+            name="title"
+            onChange={handleSubmit}
+            required={true}
+            value={FormData.title}
+             />
+          </section>
         </form>
 
       </div>
