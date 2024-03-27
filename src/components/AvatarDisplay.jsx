071244db9362468631d1a63de0771b17ca670863
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-03-27 20:58:27 +0100

    added space

--- src/components/AvatarDisplay.jsx
+++ src/components/AvatarDisplay.jsx
@@ -1,6 +1,7 @@
 const blankAvatar = 'https://cdn.pixabay.com/photo/2013/07/12/13/22/smiley-146922_1280.png'
 
 const AvatarDisplay = ({ ticket }) => {
+  
   return (
     <div className="avatar-container">
     <div className="img-container">
