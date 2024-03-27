10958d1337c80f71cb1b7937f3895cb318a28a2e
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-03-27 20:57:33 +0100

    added space to nav code

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,6 +10,7 @@ const Nav = () => {
         <div className="logo-container">
           <img className="logo-img" src={'https://cdn.pixabay.com/photo/2016/08/26/15/54/checklist-1622517_1280.png'} alt="Todoo block - checket list" />
         </div>
+        
         <div className="controls-container">
           <div className="nav-icon" onClick={() => navigate('/')}><AiOutlineInbox /></div>
           <div className="nav-icon" onClick={() => navigate('/ticket')}><AiFillBackward /></div>
